97a5827a2f523fcd1c52347ac786014a7798a8a4
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-02 14:15:48 -0600

    Implement getComputerChoice function to return random play

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+// randomly return rock, paper, or scissors
+function getComputerChoice() 
+{
+    let choices = ['rock', 'paper', 'scissors'];
+    // random integer between 0 and 3
+    let idx = Math.floor(Math.random() * 3);
+    return choices[idx];
+}
+
+console.log(getComputerChoice())
