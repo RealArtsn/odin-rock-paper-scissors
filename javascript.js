7a6d0ce5a53631cc5f24e4bea588a57204c20cce
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-02 14:48:55 -0600

    Implement function to play one round

--- javascript.js
+++ javascript.js
@@ -14,4 +14,44 @@ function getPlayerChoice()
     return prompt(message).toLowerCase();
 }
 
-console.log(getPlayerChoice())
+// return 0 if player wins, 1 if computer wins, -1 if tie
+function findWinner(playerChoice, computerChoice)
+{
+    // tie
+    if (playerChoice === computerChoice)
+    {
+        return -1
+    }
+    // find winner
+    if (computerChoice === 'rock')
+    {
+        return (playerChoice === 'paper') ? 0 : 1;
+    }
+    if (computerChoice === 'paper')
+    {
+        return (playerChoice === 'scissors') ? 0 : 1;
+    }
+    if (computerChoice === 'scissors')
+    {
+        return (playerChoice === 'rock') ? 0 : 1;
+    }
+}
+
+// play a round of rock paper scissors and return true if player won
+function playRound() 
+{
+    // get player and computer choice
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    // return string based on winner
+    switch(findWinner(playerChoice, computerChoice))
+    {
+        case -1:
+            return `It's a tie! ${playerChoice} vs ${computerChoice}`;
+        case 0:
+            return `You win! ${playerChoice} beats ${computerChoice}`;
+        case 1:
+            return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+alert(playRound());
